bc907e232273ba665442b165b424198eef1ce3a8
gokloud-ypawar <yogesh.pawar@gokloud.io>
2022-04-22 20:18:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+   LoginForm= new FormGroup({
+    Email:new FormControl("",[Validators.required,Validators.email]),
+    Remember :new FormControl(),
+    Password:new FormControl("",[Validators.required,Validators.minLength(7)])
+  })
+
   title = 'senwell-task';
+
+  loginForm=()=>{
+  console.log("Email id ",this.LoginForm.value.Email)
+  console.log("password",this.LoginForm.value.Password)
+
+  console.log(" is remember ",this.LoginForm.value.Remember)
+
+  }
+
 }
+
